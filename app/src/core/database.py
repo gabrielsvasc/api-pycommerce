5b1df5ac04222d3f78c8083466b6102b7d4a0333
Gabriel Vasconcelos <gabriel.svasc99@gmail.com>
2024-09-15 18:41:02 -0300

    ✨ add api core database and settings

---
+++ app/src/core/database.py
@@ -0,0 +1,24 @@
+import settings
+
+from sqlalchemy import URL
+from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy.engine import create_engine
+
+
+DB_URL = URL.create(
+    drivername=settings.DB_ENGINE,
+    username=settings.DB_USER,
+    password=settings.DB_PASSWORD,
+    host=settings.DB_HOST,
+    database=settings.DB_NAME,
+    port=settings.DB_PORT
+)
+ENGINE = create_engine(DB_URL)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def create_session() -> Session:
+    return Session(bind=ENGINE, autoflush=False)
